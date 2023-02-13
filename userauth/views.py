7a6d50910a5d7ff242f135bf17daff4e0a168d3f
Lolita <97053333+Lolita-codes@users.noreply.github.com>
2023-02-13 20:08:32 +0100

    Set up Login, Signup and Password reset

--- userauth/views.py
+++ userauth/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def profile_view(request):
+    return render(request, 'account/profile.html')
+
+
