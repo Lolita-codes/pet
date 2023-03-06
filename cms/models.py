3c4d4e8faf256e92b8c21d1ff1c6297332e75bff
Lolita <97053333+Lolita-codes@users.noreply.github.com>
2023-03-06 17:14:37 +0100

    Add Themes as snippets to articles

--- cms/models.py
+++ cms/models.py
@@ -1,8 +1,11 @@
+from django import forms
 from django.db import models
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel, PageChooserPanelExtend
+from modelcluster.fields import ParentalManyToManyField
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, PageChooserPanel
 from wagtail import blocks
 from wagtail.fields import RichTextField, StreamField
-from wagtail.models import Page
+from wagtail.models import Page, TranslatableMixin, Locale
+from wagtail.snippets.models import register_snippet
 
 from cms.blocks import InlineImageBlock, InlineVideoBlock
 
@@ -10,6 +13,22 @@ from cms.blocks import InlineImageBlock, InlineVideoBlock
 class HomePage(Page):
     template = "home_page.html"
     intro = RichTextField(blank=True)
+    theme_section_title = models.CharField(
+        null=True,
+        blank=True,
+        max_length=255,
+        help_text="Title to display above the theme section",
+    )
+    theme_section_intro = RichTextField(blank=True)
+    theme_section = models.ForeignKey(
+        Page,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="Featured section for the homepage. Will display all themes.",
+        verbose_name="Theme section",
+    )
     article_section_title = models.CharField(
         null=True,
         blank=True,
@@ -30,10 +49,47 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname='full'),
         MultiFieldPanel([
+            FieldPanel('theme_section_title'),
+            FieldPanel('theme_section_intro', classname='full'),
+            PageChooserPanel('theme_section'),
+        ], heading='Theme section', classname='collapsible'),
+        MultiFieldPanel([
             FieldPanel('article_section_title'),
             FieldPanel('article_section_intro', classname='full'),
             PageChooserPanel('article_section'),
         ], heading="Article section", classname='collapsible'),
+    ]
+
+
+@register_snippet
+class Theme(TranslatableMixin, models.Model):
+    name = models.CharField(max_length=255)
+
+    panels = [
+        FieldPanel('name'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+
+class ThemePage(Page):
+    theme = models.ForeignKey(Theme, on_delete=models.SET_NULL, null=True, related_name='themepages')
+    intro = RichTextField(blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image', blank=True, null=True, on_delete=models.SET_NULL, related_name='+'
+    )
+    caption = models.CharField(blank=True, null=True, max_length=255)
+
+    def articlepages(self):
+        print('hi')
+        return self.theme.articlepages.filter(locale=self.locale_id).live().order_by('-first_published_at')
+
+    content_panels = Page.content_panels + [
+        FieldPanel('theme'),
+        FieldPanel('intro', classname='full'),
+        FieldPanel('image'),
+        FieldPanel('caption'),
     ]
 
 
@@ -42,6 +98,7 @@ class ArticlePage(Page):
     image = models.ForeignKey(
         'wagtailimages.Image', blank=True, null=True, on_delete=models.SET_NULL, related_name='+', verbose_name="Image"
     )
+    themes = ParentalManyToManyField(Theme, blank=True, related_name='articlepages', verbose_name="Themes")
     featured = models.BooleanField(default=False)
     body = StreamField([
         ('paragraph', blocks.RichTextBlock(
@@ -51,11 +108,28 @@ class ArticlePage(Page):
         ('video', InlineVideoBlock()),
     ], use_json_field=True)
 
+    def themepages(self):
+        ans = ThemePage.objects.filter(theme__in=self.themes.all(), locale=self.locale_id)
+        print(ans)
+        return ans
+
     content_panels = Page.content_panels + [
         FieldPanel('intro'),
+        FieldPanel('themes', widget=forms.CheckboxSelectMultiple),
         FieldPanel('featured'),
         FieldPanel('image'),
         FieldPanel('body'),
+    ]
+
+
+class ThemeIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    # Specifies that only ThemePage objects can live under this index page
+    subpage_types = ['ThemePage']
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname='full'),
     ]
 
 
@@ -75,4 +149,3 @@ class ArticleIndexPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname='full'),
     ]
-
